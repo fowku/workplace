3418a7a5e1b58d357490a78d2690cc6270d296b6
fowku <chek970@gmail.com>
2020-02-02 00:05:38 +0300

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -6,44 +6,58 @@ module.exports = {
 
   devServer: {
     contentBase: './dist',
-    port: '3000'
+    port: '3000',
   },
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
 
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.min.js'
+    filename: 'bundle.min.js',
   },
 
   module: {
     rules: [
       {
-        test: /\.(ts)x?$/,
+        test: /\.(ts|tsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              eslintPath: require.resolve('eslint'),
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+        exclude: /node_modules/,
+      },
+
+      {
+        test: /\.(ts|tsx)$/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
 
       {
         test: /\.scss/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
 
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: {
-          loader: 'file-loader'
-        }
-      }
-    ]
+          loader: 'file-loader',
+        },
+      },
+    ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
-  ]
+      template: './public/index.html',
+    }),
+  ],
 };
